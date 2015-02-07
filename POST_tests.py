4600501352d1e20a97948455df990ea53ba3f06c
robrant <rich.brantingham@me.com>
2015-02-07 13:28:10 +0000

    Working POST and check in place for get

--- POST_tests.py
+++ POST_tests.py
@@ -1,14 +1,18 @@
 import os
+import json
 import app
 import config
 import unittest
 from mongoengine import connect
+from pymongo import MongoClient
+client = MongoClient()
+
 
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
         app.app.config['TESTING'] = True
-        app.app.config["MONGODB_DB"] = 'xxx'
+        #app.app.config["MONGODB_DB"] = 'dfkm'
         connect(
             config.MONGO_DBNAME,
             host=config.MONGO_HOST,
@@ -24,8 +28,30 @@ class FlaskrTestCase(unittest.TestCase):
         print response
 
     def test_POST_simple_point(self):
-        response = self.app.get('/')
-        print response
+        
+        data = {'zone_name' : 'test zone',
+                'description' : 'new dangerous zone',
+                'loc' : {'type' : 'Point',
+                         'coordinates': [ 40, 5 ]},
+                'radius': 10,
+                }
+        
+        data = json.dumps(data)
+        
+        res = self.app.post('/api/v1.0/zones',
+                             data=data,
+                             content_type='application/json')
+        db = client['dfkm']
+        coll = db['zones']
+        res = coll.find()
+        for x in res:
+            print x
+        
+    #def test_get(self):
+        
+
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 
 from flask import Flask, render_template, jsonify, request
 from flask import abort
+from flask import Response
 
 from flask.ext.pymongo import PyMongo
 from config import MONGO_DBNAME, MONGO_PORT
+import json
 
 app = Flask(__name__)
 mongo = PyMongo(app, config_prefix='MONGO')
@@ -44,23 +46,55 @@ def getzones():
 @app.route('/api/v1.0/zones', methods=['POST'])
 def create_zone():
 
-    if not request.json:
-        abort(400)
-    else:
-        loc = request.json['loc']
-        loc_type = request.json['loc_type']
+    # Setup a zone object to insert
+    zone = {}
 
-    zone = {
-            'zone_name': request.json['zone_name'],
-            'description': request.json.get('description', ""),
-            'loc' : loc
-            
-            #'risk' : PLACEHOLDER FOR GETTING THE RISK SCORE
-            }
+    # Ensure post's Content-Type is supported
+    if request.headers['content-type'] == 'application/json':
+        # Ensure data is a valid JSON
+        try:
+            content = json.loads(request.data)
+        except ValueError:
+            return Response(status=405)
+
+    try:
+        loc = content['loc']
+        zone['loc'] = loc
+        
+    except:
+        return Response(status=405)
+
+    if loc['type'].lower() == 'point' and content.has_key('radius') == True:
+        zone['radius'] = content['radius']
+    
+    # Default on the risk score
+    if content.has_key('risk') == True:
+        zone['risk'] = content['risk']
+    else:
+        zone['risk'] = 0
+
+    # Default on the zone name
+    if content.has_key('zone name') == True:
+        zone['zone_name'] = content['zone_name']
+    else:
+        zone['zone_name'] = 'placeholder for zone name'
+
+    # Description
+    if content.has_key('description') == True:
+        zone['description'] = content['description']
+    else:
+        zone['description'] = 'new zone'
 
     # Now push to mongo
     zone_id = mongo.db.zones.insert(zone)
-    #return jsonify({'zone_id': zone_id}), 201
+    if len(str(zone_id)) == 32:
+        response = {'zone_id' : str(zone_id) }
+
+    res = mongo.db.zones.find()
+    for x in res:
+        print x
+        
+    return jsonify({'zone_id': str(zone_id)}), 201
 
 
 if __name__ == '__main__':
